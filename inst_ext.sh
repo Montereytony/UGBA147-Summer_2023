69d855f31ab60d86b34cef34541837745a1e9dc6
Montereytony <montereytony@gmail.com>
2023-05-13 08:56:08 -0700

    Now need to push

--- inst_ext.sh
+++
@@ -1,5 +0,0 @@
-pip install jupyter_nbextensions_configurator
-jupyter nbextension install --sys-prefix --py jupyter_nbextensions_configurator --overwrite
-jupyter nbextension enable --sys-prefix --py jupyter_nbextensions_configurator
-jupyter serverextension enable --sys-prefix --py jupyter_nbextensions_configurator
-jupyter contrib nbextensions install --user
