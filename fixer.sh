692045920758b989394a0355e175a1ee85aaec55
Montereytony <montereytony@gmail.com>
2023-05-24 21:32:02 -0700

    Extensions were NFG,added script to run at login

---
+++ fixer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pip install jupyter_nbextensions_configurator
+jupyter nbextension install --sys-prefix --py jupyter_nbextensions_configurator --overwrite
+jupyter nbextension enable --sys-prefix --py jupyter_nbextensions_configurator
+jupyter serverextension enable --sys-prefix --py jupyter_nbextensions_configurator
+jupyter contrib nbextensions install --user
